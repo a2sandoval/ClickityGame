b8e1ee0ce879fb39f0f76c699425c861e5ba7fff
01100001 <a01100001@macbook-pro-7.attlocal.net>
2018-10-20 20:32:17 -0700

    Updates

---
+++ precache-manifest.070d0fd6626f4271f1cd9fe99250fc62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2d812c1eeafe83f22d",
+    "url": "/ClickityGame/static/js/runtime~main.ec2d812c.js"
+  },
+  {
+    "revision": "7d8fde0c670c00cd2403",
+    "url": "/ClickityGame/static/js/main.7d8fde0c.chunk.js"
+  },
+  {
+    "revision": "8aa5ae1d08621737ce3d",
+    "url": "/ClickityGame/static/js/1.8aa5ae1d.chunk.js"
+  },
+  {
+    "revision": "7d8fde0c670c00cd2403",
+    "url": "/ClickityGame/static/css/main.54cceff5.chunk.css"
+  },
+  {
+    "revision": "4ae5e271fbb273fc7f277bfc47e4b2a9",
+    "url": "/ClickityGame/index.html"
+  }
+];
